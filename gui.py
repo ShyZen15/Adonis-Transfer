f8b8c53f204169ece0944533f951f1691142b0e2
ShyZen <shyzen.code@gmail.com>
2023-10-06 22:01:18 +0530

    Organsied + Added User Login

---
+++ gui.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import ttk
+import customtkinter
+
+
+root = tk.Tk()
+root.geometry("500x800")
+root.resizable(0,0)
+root.title("Adonis Transfer 🗿")
+
+heading = Label(
+    a,
+    text = "Welcome to Adonis Transfer",
+
+)
+
+
+
+
+root.mainloop()
