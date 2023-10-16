87c42e31f921bc1f11424c4a2eb6b930d9225528
ShyZen15 <sshivensharma07@gmail.com>
2023-10-16 20:32:00 +0530

    Added fields

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import tkinter as tk
-from PIL import ImageTk
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
+from PIL import ImageTk
 
 root = tk.Tk()
 root.title("Adonis Transfer")
@@ -12,15 +12,38 @@ root.configure(bg="#1E1E1E")
 
 
 class functions():
+
+    def getCred(self, event=None):
+        print("NIGGA")
+
     def Login(self, event=None):
+        obj = functions()
         canvas.pack_forget()
         canv = Canvas(root, bg="#1E1E1E", width=500, height=800)
-        Text = canv.create_image(150, 100, image=LoginText, anchor=NW)
+        canv.create_image(160, 100, image=LoginText, anchor=NW)
+
+        emailIn = Entry(
+            canv, width=20,
+            bg="#423F3F",
+            fg="#FFFFFF",
+            font=("Arial", 16)
+        )
+
+        emailWin = canv.create_window(110, 250, window=emailIn, anchor=NW)
+
+        passwordIn = Entry(
+            canv, width=20,
+            bg="#423F3F",
+            fg="#FFFFFF",
+            font=("Arial", 16)
+        )
+
+        passwordWin = canv.create_window(110, 350, window=passwordIn, anchor=NW)
+
+        loginBtn = canv.create_image(150, 450, image=loginImg, anchor=NW)
+        canv.tag_bind(loginBtn, "<Button-1>", obj.getCred)
 
         canv.pack()
-
-
-
 
     def Register(self, event=None):
         print("Nigga")
@@ -40,7 +63,6 @@ RegisterImg = ImageTk.PhotoImage(file="assets/Register.png")
 Field = ImageTk.PhotoImage(file="assets/Rectangle 6.png")
 LoginText = ImageTk.PhotoImage(file="assets/LoginText.png")
 
-
 # App icon
 root.iconphoto(False, ICON)
 
@@ -50,7 +72,6 @@ loginBtn = canvas.create_image(150, 400, image=loginImg, anchor=NW)
 registerBtn = canvas.create_image(150, 600, image=RegisterImg, anchor=NW)
 canvas.tag_bind(loginBtn, "<Button-1>", obj.Login)
 canvas.tag_bind(registerBtn, "<Button-1>", obj.Register)
-
 
 canvas.pack()
 
